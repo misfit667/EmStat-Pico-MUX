5f9d4ac5f9b6c7144cb78d4044ece5781b6fbd1f
Moonshine the P <68279135+MoonshinetheP@users.noreply.github.com>
2022-11-03 11:30:34 +0100

    Editing the code to use multiplexer class

--- code.py
+++ code.py
@@ -2,12 +2,13 @@
 import time
 import pandas as pd
 import numpy as np
-#import piecewise_regression
+import piecewise_regression
 from scipy.stats import linregress
 import matplotlib.pyplot as plt
 import multiplexermanager as mm
 import fileopener as fo
 import plotting as pg
+
 
 '''START TIME'''
 # Measures the time at which the code is executed
@@ -15,30 +16,19 @@ start = time.time()
 
 path = 'C:/Users/SLinf/Documents/GitHub/EmStat-Pico-MUX/'
 file = 'data.csv'
-
+filetwo = 'test.csv'
 np.seterr(divide = 'ignore')
 
 array = fo.Multiplexer(path + file)
-
-def logarithm(input):
-    working = input.array
-    output = np.empty((working.shape))
-    for x in range(0,working.shape[0]):
-        for y in range(0,working.shape[1]):
-            if input.bipot == True:
-                output[x,y,0,:] = working[x,y,0,:]
-                output[x,y,1,:] = np.log10(np.absolute(working[x,y,1,:]))
-                output[x,y,2,:] = working[x,y,2,:]
-                output[x,y,3,:] = np.log10(np.absolute(working[x,y,3,:]))
-
-            else:
-                output[x,y,0,:] = working[x,y,0,:]
-                output[x,y,1,:] = np.log10(np.absolute(working[x,y,1,:]))
-    return output
+arraytwo = fo.Multiplexer(path + filetwo)
 
 values = logarithm(array)
-pg.compare_channels(array.array)
 
+pg.compare_experiments(values)
+pg.compare_channels(arraytwo.array)
 '''END TIME''' 
 finish = (time.time() - start)
 print('Time to run: ' + str(finish))
+
+
+
