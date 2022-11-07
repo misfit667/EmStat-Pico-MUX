70cd7042268fb2e7bf94d8d325888687116e4d88
Steven Linfield <S.Linfield@outlook.com>
2022-11-07 14:22:22 +0100

    Editing files to check multiplexer-manager use

--- code.py
+++ code.py
@@ -2,10 +2,12 @@
 import time
 import pandas as pd
 import numpy as np
-import piecewise_regression
+#import piecewise_regression
 from scipy.stats import linregress
 import matplotlib.pyplot as plt
 import multiplexermanager as mm
+import operations as op
+import plotting as pg
 
 
 '''START TIME'''
@@ -17,13 +19,22 @@ file = 'data.csv'
 filetwo = 'test.csv'
 np.seterr(divide = 'ignore')
 
-array = mm.Multiplexer(path + file)
-arraytwo = mm.Multiplexer(path + filetwo)
+array = mm.Multiplexer(path + file).array
+logged = op.logarithm(array)
+#smoothed = op.smooth(logged,500)
+tested = op.blah(logged)
+#pg.compare_channels(tested)
 
-values = mm.logarithm(array)
+fig = plt.figure(num = 1, figsize = (6.4, 4.8), dpi = 100, facecolor = 'white', edgecolor = 'white', frameon = True)
+ax = fig.add_axes([0.2, 0.2, 0.7, 0.7])
+ax.set_title('Channel ', loc = 'center', pad = 20, fontsize = 15)
+            
+ax.plot(logged[0,0,0,:], logged[0,0,1,:], linewidth = 1, linestyle = '-', color = 'green', marker = None, label = None)
+ax.plot(logged[0,0,2,:], logged[0,0,3,:], linewidth = 1, linestyle = '-', color = 'red', marker = None, label = None)
+ax.plot(tested[0,0,2,:], (tested[0,0,3,:]), linewidth = 1, linestyle = '-', color = 'blue', marker = None, label = None)
 
-mm.compare_experiments(values)
-mm.compare_channels(arraytwo.array)
+plt.show()
+plt.close()
 
 '''END TIME''' 
 finish = (time.time() - start)
